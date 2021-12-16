db9c0d3c83efc9d99a8b822aa9985396f7943bc9
KirSaxhlil <75578152+KirSaxhlil@users.noreply.github.com>
2021-12-16 14:35:25 +0300

    Update Lab5.cs

--- Lab5.cs
+++ Lab5.cs
@@ -9,9 +9,12 @@ namespace Lab5
             int N = 3;
             int I, J;
             int k = 1;
-            double[,] A = { { 1,2,3 },
-                            { 2,5,6 },
-                            { 3,6,9 } };
+            double[,] A = { { 5,1,2 },
+                            { 1,4,1 },
+                            { 2,1,3 } };
+            //double[,] A = new double[N, N];
+            //for (int i = 0; i < N; i++)
+            //    for (int j = 0; j < N; j++) A[i, j] = 1;
             double[,] H = new double[N, N];
             double max;
             double e = 0.0001;
@@ -25,11 +28,12 @@ namespace Lab5
                 I = 0; J = 1;
                 for (int i = 0; i < N; i++)
                 {
-                    for (int j = i + 1; j < N; j++)
+                    for (int j = 0; j < N; j++)
                     {
                         if (Math.Abs(A[i, j]) > Math.Abs(max)) max = A[i, j]; I = i; J = j;
                     }
                 }
+                Console.WriteLine(max);
                 if (max <= e)
                 {
                     for (int i = 0; i < N; i++) L[i] = A[i, i];
@@ -37,7 +41,7 @@ namespace Lab5
                 }
                 else
                 {
-                    fi = Math.Atan( ( (2 * A[I, J]) / (A[I, I] - A[J, J]) ) / 2);
+                    fi = Math.Atan( (2 * A[I, J]) / (A[I, I] - A[J, J]) )/2;
                     for (int i = 0; i < N; i++)
                     {
                         for (int j = 0; j < N; j++)
@@ -45,20 +49,22 @@ namespace Lab5
                             if (i == I && j == J) H[i, j] = -Math.Sin(fi);
                             else if (i == J && j == I) H[i, j] = Math.Sin(fi);
                             else if (i == j && (i == I || i == J)) H[i, j] = Math.Cos(fi);
+                            else if (i == j) H[i, j] = 1;
                             else H[i, j] = 0;
                         }
                     }
-
+                    //Output(Multiply(Transparent(H), A));
                     A = Multiply(Multiply(Transparent(H), A), H);
                     k++;
                 }
-            } while (true);
+            } while (k!=2);
 
+            
             for (int i = 0; i < N; i++) Console.Write(L[i] + "\t");
         }
         static double[,] Multiply(double[,] A, double[,] B)
         {
-            int N = A.Rank;
+            int N = A.Rank+1;
             double[,] C = new double[N, N];
             for (int i = 0; i < N; i++)
             {
@@ -84,7 +90,7 @@ namespace Lab5
 
         static double[,] Transparent(double[,] A)
         {
-            int N = A.Rank;
+            int N = A.Rank+1;
             double t;
             for (int i = 0; i < N; i++)
             {
@@ -97,6 +103,19 @@ namespace Lab5
             }
 
             return A;
+        }
+
+        static void Output(double[,] A)
+        {
+            int N = A.Rank + 1;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(A[i, j] + " ");
+                    if (j == N - 1) Console.Write("\n");
+                }
+            }
         }
     }
 }
