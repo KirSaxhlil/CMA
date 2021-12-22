5ac925f66b1e461cb36ab535128bf14c9c28aaad
KirSaxhlil <75578152+KirSaxhlil@users.noreply.github.com>
2021-12-23 02:53:03 +0300

    Update Lab5.cs

--- Lab5.cs
+++ Lab5.cs
@@ -4,11 +4,20 @@ namespace Lab5
 {
     class Program
     {
+
+        static void TEST()
+        {
+            double[,] A = { { 2,4,2 },
+                            { 1,4,1 },
+                            { 2,7,3 } };
+            Output(Multiply(A, Transparent(A)));
+            Output(A);
+        }
         static void Main(string[] args)
         {
             int N = 3;
             int I, J;
-            int k = 1;
+            int k = 0;
             double[,] A = { { 5,1,2 },
                             { 1,4,1 },
                             { 2,1,3 } };
@@ -17,7 +26,7 @@ namespace Lab5
             //    for (int j = 0; j < N; j++) A[i, j] = 1;
             double[,] H = new double[N, N];
             double max;
-            double e = 0.0001;
+            double e = 0.001;
             double fi;
 
             double[] L = new double[N];
@@ -28,12 +37,12 @@ namespace Lab5
                 I = 0; J = 1;
                 for (int i = 0; i < N; i++)
                 {
-                    for (int j = i+1; j < N; j++)
+                    for (int j = i + 1; j < N; j++)
                     {
-                        if (Math.Abs(A[i, j]) > Math.Abs(max)) max = A[i, j]; I = i; J = j;
+                        if (Math.Abs(A[i, j]) > Math.Abs(max)) { max = A[i, j]; I = i; J = j; }
                     }
                 }
-                Console.WriteLine(max);
+                //Console.WriteLine(I);
                 if (max <= e)
                 {
                     for (int i = 0; i < N; i++) L[i] = A[i, i];
@@ -41,7 +50,7 @@ namespace Lab5
                 }
                 else
                 {
-                    fi = Math.Atan( (2 * A[I, J]) / (A[I, I] - A[J, J]) )/2;
+                    fi = Math.Atan((2 * A[I, J]) / (A[I, I] - A[J, J])) / 2;
                     for (int i = 0; i < N; i++)
                     {
                         for (int j = 0; j < N; j++)
@@ -53,18 +62,20 @@ namespace Lab5
                             else H[i, j] = 0;
                         }
                     }
+                    //Output(H);
                     //Output(Multiply(Transparent(H), A));
                     A = Multiply(Multiply(Transparent(H), A), H);
                     k++;
                 }
             } while (true);
 
-            
-            for (int i = 0; i < N; i++) Console.Write(L[i] + "\t");
+            Console.WriteLine("K = " + k);
+            for (int i = 0; i < N; i++) Console.Write("L["+i+"] = "+L[i] + "\t");
+            Console.ReadLine();
         }
         static double[,] Multiply(double[,] A, double[,] B)
         {
-            int N = A.Rank+1;
+            int N = A.Rank + 1;
             double[,] C = new double[N, N];
             for (int i = 0; i < N; i++)
             {
@@ -76,9 +87,9 @@ namespace Lab5
 
             for (int i = 0; i < N; i++)
             {
-                for(int j = 0; j < N; j++)
+                for (int j = 0; j < N; j++)
                 {
-                    for(int k = 0; k < N; k++)
+                    for (int k = 0; k < N; k++)
                     {
                         C[i, j] += A[i, k] * B[k, j];
                     }
@@ -90,19 +101,27 @@ namespace Lab5
 
         static double[,] Transparent(double[,] A)
         {
-            int N = A.Rank+1;
+            int N = A.Rank + 1;
             double t;
+            double[,] B = new double[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    B[i, j] = A[i, j];
+                }
+            }
             for (int i = 0; i < N; i++)
             {
                 for (int j = i + 1; j < N; j++)
                 {
-                    t = A[i, j];
-                    A[i, j] = A[j, i];
-                    A[j, i] = t;
+                    t = B[i, j];
+                    B[i, j] = B[j, i];
+                    B[j, i] = t;
                 }
             }
 
-            return A;
+            return B;
         }
 
         static void Output(double[,] A)
